fedb5c1871488cb601535a3d73c6d08c0d956fbe
Adam Hrastnik <adam.hrastnik@gmail.com>
2020-06-08 00:27:40 +0200

    Plotted the learning error.

--- perceptron.py
+++ perceptron.py
@@ -17,6 +17,7 @@ if __name__ == "__main__":
     np.random.seed(1)
     weights = 2 * np.random.random([3, 1]) - 1
     outputs = np.zeros(4)
+    error_buffer = []
 
     print("Start weights:\n", weights)
 
@@ -24,8 +25,18 @@ if __name__ == "__main__":
         input_layer = training_inputs
         outputs = sigmoid(np.dot(input_layer, weights))
         error = training_outputs - outputs
+        error_buffer.append(np.abs(error.reshape(4)))
         adjust = error * sigmoid_derivative(outputs)
         weights += np.dot(input_layer.T, adjust)
 
     print("End weights:\n", weights)
     print("Output after training:\n", outputs)
+
+    # Plot the error
+    plt.figure()
+    plt.plot(np.arange(TRAIN_ITERATIONS), np.asarray(error_buffer))
+    plt.title("Input errors over iterations")
+    plt.xlabel("Iterations")
+    plt.ylabel("Error")
+    plt.legend(["Input 1", "Input 2", "Input 3", "Input 4"], loc='upper right')
+    plt.show()
